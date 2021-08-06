7e3934281943ab28968374a0b704e19fd6cf20b5
rizwanweb <rizwansoomro@zoho.com>
2021-08-06 23:08:57 +0500

    FInished Models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    email = models.EmailField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor'),
+    )
+
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    category = models.CharField(max_length=20, choices=CATEGORY)
+    description = models.TextField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for Delivey', 'Out for Delivery'),
+        ('Delivered', 'Delivered'),
+    )
+
+    customer = models.ForeignKey(Customer, on_delete=CASCADE)
+    product = models.ForeignKey(Product, on_delete=CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS)
